80e99fbbcd02a94382a0118a63ff5fffc65c3b28
developertroll <taegyu1500@gmail.com>
2023-10-16 17:33:05 +0900

    config error fix2

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/weather/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/weather" : "/",
   outputDir: "docs",
 };
